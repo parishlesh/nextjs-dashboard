34a41deda5037e1adf43efb1683ebb6dccfd4ee6
Parishlesh <parishleshfulvanshi@gmail.com>
2025-04-02 11:02:52 +0530

    next fist commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import {Inter, Lusitana, Roboto} from 'next/font/google'
+
+export const inter = Inter({subsets: ["latin", "greek"] })
+
+export const lusitana = Lusitana({
+    subsets: ["latin"],
+    weight: ['400', '700']
+})
